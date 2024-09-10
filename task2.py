3a8c9a28d05b14d29cec293de766a50e0d9ee271
knzv3248 <166550577+knzv3248@users.noreply.github.com>
2024-09-11 00:33:39 +0500

    Tasks is done
    Задачи выполнены

--- task2.py
+++ task2.py
@@ -1,10 +1,18 @@
-from  flask import Flask, render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return  f"Сегодня {cur_data}. Текущее время - {cur_time}"
+def index():
+    return render_template('index.html', title="Главная")
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', title="Блог")
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title="Контакты")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
